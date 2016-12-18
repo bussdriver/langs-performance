a07054b207193c4a2de7f304e13bb86d94e5d81d
bussdriver <john1c@fehlen.org>
2016-12-18 01:02:20 -0600

    Update primes.pl
    speed increase

--- primes.pl
+++ primes.pl
@@ -1,46 +1,43 @@
 use strict;
 use warnings;
+use Time::HiRes qw(gettimeofday);
 
 sub get_primes7($) {
-	my ($n) = @_;
+	my $n= $_[0];
 
-	if ($n < 2) { return (); }
-	if ($n == 2) { return (2); }
+	return		if $n < 2;
+	return (2)	if $n == 2;
 	# do only odd numbers starting at 3
-	my @s = ();
+	my @s;
 	for (my $i = 3; $i < $n + 1; $i += 2) {
-		push(@s, $i);
+		push @s, $i;
 	}
 	# n**0.5 simpler than math.sqr(n)
 	my $mroot = $n ** 0.5;
 	my $half = scalar @s;
 	my $i = 0;
 	my $m = 3;
+	my $j;
 	while ($m <= $mroot) {
 		if ($s[$i]) {
-			my $j = int(($m*$m - 3) / 2);
+			$j = int(($m*$m - 3) * 0.5);
 			$s[$j] = 0;
 			while ($j < $half) {
 				$s[$j] = 0;
 				$j += $m;
 			}
 		}
-		$i = $i + 1;
-		$m = 2*$i + 3;
+		$m = 2* ++$i + 3;
 	}
-	my @res = (2);
-	foreach (@s) {
-		push(@res, $_) if ($_);
-	}
-	return @res;
+	return grep {$_} 2,@s;
 }
 
-my $startTime = time();
+my $startTime = gettimeofday();
 my $periodTime = $ENV{'RUN_TIME'};
 
 my @res;
 
-while ((time() - $startTime) < $periodTime) {
+while (gettimeofday() - $startTime < $periodTime) {
 	@res = get_primes7(10000000);
-	print "Found ".(scalar @res)." prime numbers.\n";
+	print "Found $#res prime numbers.\n";
 }
